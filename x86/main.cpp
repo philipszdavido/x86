a52af2f654efd1f4cb62bdfed461abd1f63bcbc9
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-03-19 18:46:04 +0100

    Initial Commit

---
+++ x86/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  x86
+//
+//  Created by Chidume Nnamdi on 19/03/2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
